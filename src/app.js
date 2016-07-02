73b27d3f1ead39b0c9a982b82de7ee1905121284
Natalie Macias <nramirez787@gmail.com>
2016-07-01 16:27:47 -1000

    new file structure moved app.js file

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+import React from 'react';
+
+const Link = ReactRouter.Link
+
+const App = React.createClass({
+  render(){
+    return (
+      <div className= "myapp">
+        <h1> My App </h1>
+      </div>
+    )
+  }
+})
